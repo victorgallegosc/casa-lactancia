c1428a3c8600c3cf16fb6133cf96b5894022d279
victorgallegosc <victorgc11@outlook.com>
2025-03-15 14:21:12 -0600

    Divide home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,35 +1,12 @@
-import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import {
-      CarouselComponent,
-      CarouselInnerComponent,
-      CarouselItemComponent,
-      ThemeDirective,
-} from '@coreui/angular';
+import { CarouselComponent } from '../carousel/carousel.component';
+import { IntroComponent } from '../intro/intro.component';
 
 @Component({
       selector: 'app-home',
-      imports: [
-            NgFor,
-            ThemeDirective,
-            CarouselComponent,
-            CarouselInnerComponent,
-            CarouselItemComponent,
-      ],
+      standalone: true,
+      imports: [CarouselComponent, IntroComponent],
       templateUrl: './home.component.html',
       styleUrl: './home.component.css',
 })
-export class HomeComponent {
-      slides: any[] = [
-            {
-                  id: 1,
-                  src: './assets/img/carousel/img-1.avif',
-            },
-            {
-                  id: 1,
-                  src: './assets/img/carousel/img-2.avif',
-            },
-      ];
-
-      ngOnInit() {}
-}
+export class HomeComponent {}
