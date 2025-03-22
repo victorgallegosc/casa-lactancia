529fdbae2d230b4d3cc95f89156233ae1e947fc3
victorgallegosc <victorgc11@outlook.com>
2025-03-22 15:27:00 -0600

    Refactor header component structure and styles for improved layout and responsiveness

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
       selector: 'app-header',
-      imports: [MenuComponent],
+      standalone: true,
+      imports: [CommonModule, MenuComponent],
       templateUrl: './header.component.html',
       styleUrl: './header.component.css',
 })
