aa2a665bd52201ada0d4fb052ad84381b6187913
victorgallegosc <victorgc11@outlook.com>
2025-03-22 15:27:47 -0600

    Add academic formation component to home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { IntroComponent } from '../intro/intro.component';
+import { AcademicFormationComponent } from "../academic-formation/academic-formation.component";
 
 @Component({
       selector: 'app-home',
       standalone: true,
-      imports: [CarouselComponent, IntroComponent],
+      imports: [CarouselComponent, IntroComponent, AcademicFormationComponent],
       templateUrl: './home.component.html',
       styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent { }
